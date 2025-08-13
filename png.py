b5eb8957d1853e037f598291225a613fda6be389
Barni-Mezei <mezeibg@gmail.com>
2025-08-13 15:20:32 +0200

    Started working on a png reading function

--- png.py
+++ png.py
@@ -7,30 +7,31 @@ TODO:
 
 - make it  apython module, with a single class
 
+Resources:
+https://www.nayuki.io/page/png-file-chunk-inspector
+
 """
 
 import zlib
+import time
 
 # Flags
 PNG_READ = 1 << 0 # Image reading mode
-PNG_WRITE = 1 << 1 # Image writing mode (creating images)
-PNG_COLOR_RGBA = 1 << 2 # Truecolor mode
-PNG_COLOR_PALETTE = 1 << 3 # Plette mode
-PNG_INPUT_MATRIX = 1 << 4 # Input is a 2d array
-PNG_INPUT_ARRAY = 1 << 5 # Input is a 1d array
+PNG_COLOR_PALETTE = 1 << 1 # Plette mode
+PNG_INPUT_ARRAY = 1 << 2 # Input is a 1d array
 
 class PNG:
-    log_level : int = 0
+    log_level : int = 1
 
     flags : int
     image_data : list
     palette : list
-    image_width : int
-    image_height : int
+    image_meta : dict
 
     _file_data : bytearray
+    _was_modified : bool
 
-    def __init__(self, flags : int = PNG_COLOR_RGBA|PNG_INPUT_MATRIX, image_data : list = [], width : int = None, height : int = None, palette : list|None = None) -> None:
+    def __init__(self, image_data : list = [], width : int = None, height : int = None, palette : list|None = None, flags : int = 0, ) -> None:
         """
         **Description:**
         
@@ -48,38 +49,64 @@ class PNG:
 
         **Possible flags:**
         - PNG_READ: The constructor is in image reading mode, meaning, the object expects only a file name, to read, and process later
-        - PNG_WRITE: The constructor is in image wriring mode, this is used for creating new images
-        - PNG_COLOR_RGBA: The image will use true color + alpha (1 byte for R, G, B and A, each.) *Pixel values expected to be a 4 items long array*
+        - PNG_WRITE (**default**): The constructor is in image wriring mode, this is used for creating new images
+        - PNG_COLOR_RGBA (**default**): The image will use true color + alpha (1 byte for R, G, B and A, each.) *Pixel values expected to be a 4 items long array*
         - PNG_COLOR_PALETTE: The image will be in palette mode. *Pixel values expected to be a single integer.*
-        - PNG_INPUT_MATRIX: The image_data must be in a matrix form (2d array, where the first dimension contains the scanlines)
+        - PNG_INPUT_MATRIX (**default**): The image_data must be in a matrix form (2d array, where the first dimension contains the scanlines)
         - PNG_INPUT_ARRAY: The image_data is expected to be an arry, containing pixel values, from top left, to top right,
         then down, mimicking scanlines.
         """
-        
+
         self.flags = flags
         self.image_data = image_data
         self.palette = palette
-        self.image_width = width
-        self.image_height = height
+        self.image_meta = {
+            "width": width,
+            "height": height,
+        }
 
         self._file_data = None
+        self._was_modified = False
 
         if len(image_data) == 0:
             raise ValueError("Image data can not be empty!")
 
-        # Set default values
-        if self.flags & PNG_INPUT_MATRIX and self.image_width == None:
-            self.image_width = len(self.image_data[0])
+        # Read mode
+        if self.flags & PNG_READ:
+            with open(image_data, "rb") as f:
+                self._file_data = f.read()
 
-        if self.flags & PNG_INPUT_MATRIX and self.image_height == None:
-            self.image_height = len(self.image_data)
+        # Write mode
+        else:
+            # Set default values if input is in matrix form
+            if not self.flags & PNG_INPUT_ARRAY and self.image_meta["width"] == None:
+                self.image_meta["width"] = len(self.image_data[0])
 
-        # Generate image data if in write mode
-        if self.flags & PNG_WRITE:
-            self._generate_image()
+            if not self.flags & PNG_INPUT_ARRAY and self.image_meta["height"] == None:
+                self.image_meta["height"] = len(self.image_data)
+
+    def fill(self, color):
+        """
+        ### READ & WRITE MODE
+
+        **Description:**
+
+        Fills the entire image with the specified color.
+
+        **Parameters:**
+        - color(tuple): (r : int, g : int, b : int, a : int) The color to fill with
+        """
+
+        self.image_data = [[color for x in range(self.image_meta["width"])] for y in range(self.image_meta["height"])]
+        self._file_data = None
+        self._was_modified = True
+
+        pass
 
     def write(self, file_name : str) -> None:
         """
+        ### READ & WRITE MODE
+
         **Description:**
         
         The function will crate a new file, with the specified name (overwriting previous files) and puts the image into it, as a valid PNG image
@@ -87,8 +114,9 @@ class PNG:
         **Parameters:**
         - file_name(str) The name of the file, where the image data will be written into.
         """
+
         if not self._file_data:
-            raise ValueError("No image data found!")
+            self._file_data = self._generate_image()
 
         f = open(file_name, "wb")
         f.write(self._file_data)
@@ -96,17 +124,40 @@ class PNG:
 
     def get_bytes(self) -> bytearray:
         """
+        ### READ & WRITE MODE
+
         **Description:**
 
         Returns with the raw bytes read from the image.
         """
+
         if not self._file_data:
-            raise ValueError("No image data found!")
+            self._file_data = self._generate_image()
 
         return self._file_data
 
+    def get_matrix(self) -> list:
+        """
+        ### READ MODE
+
+        **Description:**
+
+        Returns with a 2d matrix of RGBA colors, read from the image.
+        """
+
+        if self._was_modified:
+            return self.image_data
+        else:
+            self.get_meta()
+        if not self._file_data: self._file_data = self._generate_image()
+
+        return self._file_data
+
+
     def get_meta(self) -> dict:
         """
+        ### READ MODE
+
         **Description:**
 
         Returns with a dictionary containing metadata about the read image. The structure looks like this:
@@ -121,6 +172,8 @@ class PNG:
 
     def shader(self, callback) -> None:
         """
+        ### READ & WRITE MODE
+
         **Description:**
 
         This function will itrate over every pixel in the image, calling the provided callback function on every pixel, essentially acting a a shader.
@@ -137,10 +190,241 @@ class PNG:
             - output_color(touple): (r : int, g : int, b : int, a: int)
         """
 
+        self._was_modified = True
+
         pass
 
 
 
+    def _read_image_data(self) -> tuple:
+        """
+        **Description:**
+
+        Reads the image dtaa from self._file_data and parses it, retrieving IHDR metadata and color data
+
+        """
+
+        if not self._file_data:
+            raise ValueError("No image data found!")
+
+        magic_header = bytearray([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
+
+        buffer = self._file_data
+
+        # Check for the magic header
+        if buffer[:len(magic_header)] == magic_header:
+            buffer = buffer[len(magic_header):]
+        else:
+            raise ValueError("Invalid PNG image (Invalid header)")
+
+        out = {
+            "size": len(self._file_data),
+            "chunks": {},
+        }
+
+        while len(buffer) > 0:
+            chunk_length = int.from_bytes(buffer[:4])
+            buffer = buffer[4:]
+
+            chunk_type = str(buffer[:4], encoding="ascii")
+            buffer = buffer[4:]
+
+            chunk_data_bytes = buffer[:chunk_length]
+            buffer = buffer[chunk_length:]
+
+            chunk_crc = buffer[:4]
+            buffer = buffer[4:]
+
+            out["chunks"][chunk_type] = {
+                "length": chunk_length,
+                "data_bytes": chunk_data_bytes,
+                "data": {},
+                "crc": chunk_crc,
+            }
+
+        for key in out["chunks"]:
+            match key:
+                case "IHDR":
+                    chunk_data_bytes = out["chunks"]["IHDR"]["data_bytes"]
+
+                    out["chunks"]["IHDR"]["data"] = {
+                        "width": 0,
+                        "height": 0,
+                        "bit_depth": 0,
+                        "color_type": 0,
+                        "compression_method": 0,
+                        "filter_method": 0,
+                        "interlace_method": 0,
+                    }                    
+
+                    out["chunks"]["IHDR"]["data"]["width"] = int.from_bytes(chunk_data_bytes[:4])
+                    chunk_data_bytes = chunk_data_bytes[4:]
+                    out["chunks"]["IHDR"]["data"]["height"] = int.from_bytes(chunk_data_bytes[:4])
+                    chunk_data_bytes = chunk_data_bytes[4:]
+                    out["chunks"]["IHDR"]["data"]["bit_depth"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+                    out["chunks"]["IHDR"]["data"]["color_type"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+                    out["chunks"]["IHDR"]["data"]["compression_method"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+                    out["chunks"]["IHDR"]["data"]["filter_method"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+                    out["chunks"]["IHDR"]["data"]["interlace_method"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                    if self.log_level > 0: print(out["chunks"]["IHDR"]["data"])
+
+                case "IDAT":
+                    chunk_data_bytes = zlib.decompress( out["chunks"]["IDAT"]["data_bytes"], wbits=32)
+
+                    out["chunks"]["IDAT"]["data"] = {
+                        "pre_matrix": [], # Raw matrix of colors or palette indexes
+                        "filter": [], # Filter method for ecah scanline
+                        "matrix": [], # The completed color matrix, after applying the filter
+                    }
+
+                    channel_count = 1
+
+                    if out["chunks"]["IHDR"]["data"]["color_type"] == 0 or out["chunks"]["IHDR"]["data"]["color_type"] == 3: channel_count = 1
+                    if out["chunks"]["IHDR"]["data"]["color_type"] == 4: channel_count = 2
+                    if out["chunks"]["IHDR"]["data"]["color_type"] == 2: channel_count = 3
+                    if out["chunks"]["IHDR"]["data"]["color_type"] == 6: channel_count = 4
+
+                    pixel_size = int(channel_count * (out["chunks"]["IHDR"]["data"]["bit_depth"] / 8))
+
+                    for y in range(out["chunks"]["IHDR"]["data"]["height"]):
+                        scanline = []
+
+                        # Save line filter type
+                        data_offset = (y*out["chunks"]["IHDR"]["data"]["width"]) * pixel_size + y
+                        out["chunks"]["IDAT"]["data"]["filter"].append( int.from_bytes(chunk_data_bytes[data_offset:data_offset + 1]) )
+
+                        for x in range(out["chunks"]["IHDR"]["data"]["width"]):
+                            data_offset = (y*out["chunks"]["IHDR"]["data"]["width"] + x) * pixel_size + (y + 1) # For filter bytes
+
+                            raw_pixel_data = chunk_data_bytes[data_offset:data_offset + pixel_size]
+                            #chunk_data_bytes = chunk_data_bytes[pixel_size:]
+
+                            if self.log_level > 1: print(f"Read {pixel_size} bytes ({channel_count}*{int(out["chunks"]["IHDR"]["data"]["bit_depth"]/8)}) on offset {data_offset}: {list(raw_pixel_data)}")
+
+                            # Split into colors.
+                            pixel_data = [raw_pixel_data[i] for i in range(0, channel_count, 1)]
+
+                            scanline.append(pixel_data)
+
+                        out["chunks"]["IDAT"]["data"]["matrix"].append(scanline)
+                        if self.log_level > 0: print(f"Reading IDAT chunk: {y+1}/{out["chunks"]["IHDR"]["data"]["height"]}", end="\r")
+
+                    if self.log_level > 0: print(f"\n")
+
+                    # Process filters
+                    for y, scanline in enumerate(out["chunks"]["IDAT"]["data"]["matrix"]):
+                        filter_type = out["chunks"]["IDAT"]["data"]["filter"][y]
+
+                        match filter_type:
+                            case 0:
+                                # No filter
+                                pass
+
+                            case 1:
+                                # Sub filter
+                                for x, pixel in enumerate(scanline):
+                                    pixel_value = pixel
+
+                                    if x > 0:
+                                        for channel_index, channel in enumerate(pixel):
+                                            pixel_value[channel_index] += scanline[x - 1][channel_index]
+
+                                    out["chunks"]["IDAT"]["data"]["matrix"][y][x] = pixel_value
+
+                            case 2:
+                                # Up filter
+                                for x, pixel in enumerate(scanline):
+                                    pixel_value = pixel
+
+                                    if y > 0:
+                                        for channel_index, channel in enumerate(pixel):
+                                            pixel_value[channel_index] += out["chunks"]["IDAT"]["data"]["matrix"][y - 1][x][channel_index]
+
+                                    out["chunks"]["IDAT"]["data"]["matrix"][y][x] = pixel_value
+
+
+                case "tEXT":
+                    chunk_data_bytes = out["chunks"]["tEXT"]["data_bytes"]
+
+                    out["chunks"]["tEXT"]["data"] = {
+                        "key": "",
+                        "value": "",
+                    }
+
+                    destination = "key"
+
+                    while len(chunk_data_bytes) > 0:
+                        character = int.from_bytes(chunk_data_bytes[:1])
+                        chunk_data_bytes = chunk_data_bytes[1:]
+                        
+                        if character == 0:
+                            destination = "value"
+                            continue
+
+                        out["chunks"]["tEXT"]["data"][destination] += str(character, encoding="ascii")
+                
+                case "zTXt":
+                    chunk_data_bytes = out["chunks"]["zTXt"]["data_bytes"]
+
+                    out["chunks"]["zTXt"]["data"] = {
+                        "key": "",
+                        "value": "",
+                    }
+
+                    destination = "key"
+
+                    while len(chunk_data_bytes) > 0:
+                        character = int.from_bytes(chunk_data_bytes[:1])
+                        chunk_data_bytes = chunk_data_bytes[1:]
+                        
+                        if character == 0:
+                            destination = "value"
+                        
+                            compression_method = int.from_bytes(chunk_data_bytes[:1])
+                            chunk_data_bytes = chunk_data_bytes[1:]
+                        
+                            out["chunks"]["zTXt"]["data"]["compression_method"] = compression_method
+
+                            continue
+
+                        out["chunks"]["zTXt"]["data"][destination] += character
+
+                    # Decode value
+                    out["chunks"]["zTXt"]["data"][destination] += character
+
+
+
+                case "tIME":
+                    chunk_data_bytes = out["chunks"]["tIME"]["data_bytes"]
+
+                    out["chunks"]["tIME"]["data"]["year"] = int.from_bytes(chunk_data_bytes[:2])
+                    chunk_data_bytes = chunk_data_bytes[2:]
+
+                    out["chunks"]["tIME"]["data"]["month"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                    out["chunks"]["tIME"]["data"]["day"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                    out["chunks"]["tIME"]["data"]["hour"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                    out["chunks"]["tIME"]["data"]["minute"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                    out["chunks"]["tIME"]["data"]["second"] = int.from_bytes(chunk_data_bytes[:1])
+                    chunk_data_bytes = chunk_data_bytes[1:]
+
+                case "IEND":
+                    out["chunks"]["IEND"]["data"] = None
+
+        return out
 
 
     def _generate_crc(self, data : bytearray) -> int:
--- test.py
+++ test.py
@@ -21,5 +21,42 @@ image_data2 = [
     [ (0,0,0,255), (0,0,0,255), (0,0,0,255), (0,0,0,255), (0,0,0,255) ],
 ]
 
-image = PNG(PNG_WRITE|PNG_COLOR_RGBA|PNG_INPUT_MATRIX, image_data2)
-image.write("test.png")
+#image = PNG(image_data2)
+#print(image.get_meta())
+#image.write("test.png")
+
+image = PNG("test_images/brown.png", flags=PNG_READ)
+image_data = image._read_image_data()
+
+for key in image_data["chunks"]:
+    print(f"{key}:")
+
+    match key:
+        case "IHDR":
+            for data_key in image_data["chunks"][key]["data"]:
+                print(f" - {data_key}: {image_data["chunks"][key]["data"][data_key]}")
+        
+        case "IDAT":
+            for data_item in image_data["chunks"][key]["data"]["matrix"]:
+                for pixel in data_item:
+                    ansi_code = f"\033[38;2;{pixel[0]};{pixel[1]};{pixel[2]}m"
+                    reset_code = "\033[0m"
+                    print(f"{ansi_code}██", end=reset_code)
+                print()
+            print("Filter types:", image_data["chunks"][key]["data"]["filter"])
+
+        case "tEXT":
+            print(f" {image_data["chunks"][key]["data"]["key"]}: {image_data["chunks"][key]["data"]["value"]}")
+
+        case "tTXt":
+            print(f" {image_data["chunks"][key]["data"]["key"]}: {image_data["chunks"][key]["data"]["value"]}")
+            print(f" Compression method: {image_data["chunks"][key]["data"]["compression_method"]}")
+
+        case "tIME":
+            for data_key in image_data["chunks"][key]["data"]:
+                print(f" - {data_key}: {image_data["chunks"][key]["data"][data_key]}")
+
+        case "IEND":
+            print(" end")
+
+    print()
